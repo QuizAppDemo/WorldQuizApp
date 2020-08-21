88fed6407a67774f87f9df53b5681a0c3bf64454
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2020-08-21 23:32:55 +0530

    init commit

---
+++ app/src/main/java/com/example/quizapp/utils/CommonUtils.java
@@ -0,0 +1,66 @@
+package com.example.quizapp.utils;
+
+public class CommonUtils {
+    public static int SPLASH_SCREEN_TIME_OUT=2000;
+    public static String sharedPrefName="MySharedPref";
+    public static String TOTALSCORE="total_score";
+    public static  int totalScore=0;
+
+    public static String btnQuestion = "Which of the following is not associated with the UNO?";
+    public static String btnAnswer1 = "ILO";
+    public static String btnAnswer2 = "WHO";
+    public static String btnAnswer3 = "ASEAN";
+    public static String btnAnswer4 = "All of the above";
+    public static String btnCorrectAnswer = btnAnswer3;
+
+    public static String btnQuestion2 = "The chairmanship/presidency of the UN Security Council rotates among the Council Members";
+    public static String btnAnswer2_1 = "every 6 months";
+    public static String btnAnswer2_2 = "every 3 months";
+    public static String btnAnswer2_3 = "every year";
+    public static String btnAnswer2_4 = "every month";
+    public static String btnCorrectAnswer2 = btnAnswer2_4;
+
+    public static String cbQuestion = "Permanent Secretariat to coordinate the implementation of SAARC programme is located at";
+    public static String cbAnswer1 = "Dhaka";
+    public static String cbAnswer2 = "New Delhi";
+    public static String cbAnswer3 = "Colombo";
+    public static String cbAnswer4 = "Kathmandu";
+    public static String cbCorrectAnswer = cbAnswer4;
+
+    public static String cbQuestion2 = "The United Nations Conference on Trade and Development (UNCTAD) is located at which of the following places?";
+    public static String cbAnswer2_1 = "Geneva";
+    public static String cbAnswer2_2 = "Rome";
+    public static String cbAnswer2_3 = "Paris";
+    public static String cbAnswer2_4 = "Vienna";
+    public static String cbCorrectAnswer2 = cbAnswer2_1;
+
+    public static String rbQuestion = "SAARC is observing a decade(1991-2000) of which of the following?";
+    public static String rbAnswer1 = "Girl child";
+    public static String rbAnswer2 = "Literacy";
+    public static String rbAnswer3 = "Health services to rural poor";
+    public static String rbAnswer4 = "Shelter for all";
+    public static String rbCorrectAnswer = rbAnswer1;
+
+    public static String rbQuestion2 = "The headquarters of Food and Agriculture Organisation is in";
+    public static String rbAnswer2_1 = "Washington";
+    public static String rbAnswer2_2 = "Paris";
+    public static String rbAnswer2_3 = "Madrid";
+    public static String rbAnswer2_4 = "Rome";
+    public static String rbCorrectAnswer2 = rbAnswer2_4;
+
+
+    public static String rvQuestion = "Which of the following describe correctly the Group of Seven Countries (G-7)?";
+    public static String rvAnswer1 = "They are developing countries";
+    public static String rvAnswer2 = "They are industrialised countries";
+    public static String rvAnswer3 = "They are holding Atomic Bomb technology";
+    public static String rvAnswer4 = "They are countries who can launch their own satellites";
+    public static String rvCorrectAnswer = rvAnswer2;
+
+    public static String rvQuestion2 = "Which one of the following is not related to disarmament?";
+    public static String rvAnswer2_1 = "SALT";
+    public static String rvAnswer2_2 = "NPT";
+    public static String rvAnswer2_3 = "CTBT";
+    public static String rvAnswer2_4 = "NATO";
+    public static String rvCorrectAnswer2 = rvAnswer2_4;
+
+}
